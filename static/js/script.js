de45f34871e5c67d1175a2b35800a6f4c88ef549
horizons83 <euanfennell@icloud.com>
2021-05-10 15:25:52 +0000

    Corrected functionality of add/remove item buttons on add recipe page to stop all lines being removed

--- static/js/script.js
+++ static/js/script.js
@@ -4,18 +4,17 @@ $(document).ready(function(){
     $('.tooltipped').tooltip();
     $('.collapsible').collapsible();
     $('select').formSelect();
-  });
-
- // Add ingredient input line on the edit/add recipe form:
+});
+  // Add ingredient input line on the edit/add recipe form:
  $(".add-ingred").click(function () {
-   $(".ingre-input").append('<input id="ingredients" name="ingredients" type="text" placeholder="ingredient" aria-label="ingredient input" required> ');
-   $(".remove-ing").show();
+   $(".ingre-input").append('<input class=ingredients id="ingredients" name="ingredients" type="text" placeholder="ingredient" aria-label="ingredient input" required> ');
+   $(".remove-ingred").show();
  });
 
  // Add method input line on edit/add recipe form:
  $(".add-method").click(function () {
-   $(".method-input").append(' <input id="method" name="method" type="text" placeholder="Add Method Step" aria-label="method input" required> ');
-   $(".remove-met").show();
+   $(".method-input").append(' <input class="method" name="method" type="text" placeholder="Add Method Step" aria-label="method input" required> ');
+   $(".remove-method").show();
  });
 
  // Remove last ingredient  added on edit/add recipe form:
@@ -23,17 +22,17 @@ $(document).ready(function(){
    $(".ingredients").last().remove();
  });
 
- // Remove last method input on added add/edit recipe form:
  $(".remove-method").click(function () {
-   $("#method").last().remove();
-
-   // Show confirm (yes/no) delete buttons when delete recipe button is pressed:
-   $(".delete-btn").click(function () {
-     $(".del-confirm").show();
-   });
-
-   // Hide confirm (yes/no) delete buttons when "No" button is pressed:
-   $(".cancel-delete-btn").click(function () {
-     $(this).parent().hide();
-   });
+  $(".method").last().remove();
 });
+
+ $(".delete-btn").click(function () {
+   $(this).next().show();
+ });
+
+ $(".cancel-delete-btn").click(function () {
+   $(this).parent().hide();
+ });
+
+
+ 
